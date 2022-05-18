a2a086470ae73e4e795db5e2c4abac6c50353c70
KaiGeller <gellerka@oregonstate.edu>
2022-05-17 20:23:21 -0700

    1

--- count_letters.py
+++ count_letters.py
@@ -2,7 +2,6 @@
 #GitHub Username: KaiGeller
 #5/17/2022
 #This code will take the letters in a string and return the number of each letter in that string
-string = "kfnefuqwfonqliuwfqiuwhHHHHH"
 def count_letters(string):
     """This function will find the number of each letter in the word"""
     new_dictionary = {}
